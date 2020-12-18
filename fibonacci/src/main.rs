eb148bf3e91d629919883cc1beec5e8a7be95dcc
Fabian Vilers <fabian.vilers@dev-one.com>
2020-12-18 08:14:53 +0100

    Add fibonacci

---
+++ fibonacci/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+fn main() {
+    loop {
+        println!("Generate nth Fibonacci number");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let result = fibonacci(input);
+        println!("The {}th number is {}", input, result);
+    }
+}
